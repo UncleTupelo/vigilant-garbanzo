99c9fd8a709af7fef53b4bc33385bf4e698fecc5
joeflynn.eth <jayfarrar@pm.me>
2021-12-06 04:25:34 -0500

    Initialize tka-send-notifications-subtest2

---
+++ runtime/common/src/impls.rs
@@ -0,0 +1,73 @@
+// Copyright 2019-2020 Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Auxillary struct/enums for polkadot runtime.
+
+use sp_runtime::traits::Convert;
+use frame_support::traits::{OnUnbalanced, Imbalance, Currency};
+use crate::NegativeImbalance;
+
+/// Logic for the author to get a portion of fees.
+pub struct ToAuthor<R>(sp_std::marker::PhantomData<R>);
+
+impl<R> OnUnbalanced<NegativeImbalance<R>> for ToAuthor<R>
+where
+	R: balances::Trait + authorship::Trait,
+	<R as system::Trait>::AccountId: From<primitives::v0::AccountId>,
+	<R as system::Trait>::AccountId: Into<primitives::v0::AccountId>,
+	<R as system::Trait>::Event: From<balances::RawEvent<
+		<R as system::Trait>::AccountId,
+		<R as balances::Trait>::Balance,
+		balances::DefaultInstance>
+	>,
+{
+	fn on_nonzero_unbalanced(amount: NegativeImbalance<R>) {
+		let numeric_amount = amount.peek();
+		let author = <authorship::Module<R>>::author();
+		<balances::Module<R>>::resolve_creating(&<authorship::Module<R>>::author(), amount);
+		<system::Module<R>>::deposit_event(balances::RawEvent::Deposit(author, numeric_amount));
+	}
+}
+
+/// Converter for currencies to votes.
+pub struct CurrencyToVoteHandler<R>(sp_std::marker::PhantomData<R>);
+
+impl<R> CurrencyToVoteHandler<R>
+where
+	R: balances::Trait,
+	R::Balance: Into<u128>,
+{
+	fn factor() -> u128 {
+		let issuance: u128 = <balances::Module<R>>::total_issuance().into();
+		(issuance / u64::max_value() as u128).max(1)
+	}
+}
+
+impl<R> Convert<u128, u64> for CurrencyToVoteHandler<R>
+where
+	R: balances::Trait,
+	R::Balance: Into<u128>,
+{
+	fn convert(x: u128) -> u64 { (x / Self::factor()) as u64 }
+}
+
+impl<R> Convert<u128, u128> for CurrencyToVoteHandler<R>
+where
+	R: balances::Trait,
+	R::Balance: Into<u128>,
+{
+	fn convert(x: u128) -> u128 { x * Self::factor() }
+}
